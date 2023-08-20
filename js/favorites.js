aeb850c758b56babf51c0eaecaf51375703b4f4f
Ronaldo Amorim <ronaldoamorim144@gmail.com>
2023-08-19 23:26:51 -0300

    Layout atualizado e remover livro dos favoritos finalizado

--- js/favorites.js
+++ js/favorites.js
@@ -1,60 +1,58 @@
 const listBooksFavorite = JSON.parse(localStorage.getItem("books")) || [];
 
-
-
-
-listBooksFavorite.forEach(element => {
-    getAPI(element.id);
+listBooksFavorite.forEach((element) => {
+  getAPI(element.id);
 });
 
 async function getAPI(url) {
-    try {
-        const api = await fetch(url);
-        const result = await api.json();
-        createFavoriteElement(result);
-    } catch (error) {
-        console.error("Erro ao buscar os dados da API:", error);
-    }
+  try {
+    const api = await fetch(url);
+    const result = await api.json();
+    createFavoriteElement(result);
+  } catch (error) {
+    console.error("Erro ao buscar os dados da API:", error);
+  }
 }
 
 function createFavoriteElement(api) {
-    const favoriteContainer = document.createElement('div');
-    favoriteContainer.classList.add('favorite-container');
-    let image = api.volumeInfo.imageLinks.smallThumbnail;
-    let title = api.volumeInfo.title;
-    let id = api.volumeInfo.authors[0];
+  const favoriteContainer = document.createElement("div");
+  favoriteContainer.classList.add("item");
+  let image = api.volumeInfo.imageLinks.smallThumbnail;
+  let title = api.volumeInfo.title;
+  let id = api.selfLink;
 
-    favoriteContainer.innerHTML = `
+  favoriteContainer.innerHTML = `
+
+  <h3>${title}</h3>
         <img class="img-favorite" src="${image}">
         <div>
             <section class="favorite-info">
-                <h3>${title}</h3>
-                <h4>${id}</h4>
+               
+              
             </section>
-            <section class="remove-favorite">
-                <button class="remove-button">Remover</button>
+            <section class="remove-favorite" id=${id}>
+                <button class="remove-button btn btn-danger">Remover</button>
             </section>
         </div>
+   
     `;
 
-
-    const favoriteList = document.querySelector('.favorite-list');
-    favoriteList.appendChild(favoriteContainer);
-    removeFavorite(favoriteContainer, id);
+  const favoriteList = document.querySelector(".favorite-list");
+  favoriteList.appendChild(favoriteContainer);
+  removeFavorite(favoriteContainer);
 }
 
 function removeFavorite(list) {
-    const removeBook = list.querySelector('.remove-button');
-    const id = list.querySelector('#');
-  
-    removeBook.addEventListener('click', ()=> {
-        console.log('id da lista', list.childNodes[3])
-        list.remove();
+  const removeBook = list.querySelector(".remove-button");
+  const id = list.childNodes[3].childNodes[3].getAttribute("id");
 
-        
-        // listBooksFavorite.splice(listBooksFavorite.findIndex((item) => item.id === id), 1);
-        // localStorage.setItem('books', JSON.stringify(listBooksFavorite));
-        console.log((listBooksFavorite.findIndex((item) => console.log(item.id === id))))
-    })
+  removeBook.addEventListener("click", () => {
+    list.remove();
 
+    listBooksFavorite.splice(
+      listBooksFavorite.findIndex((item) => item.id === id),
+      1
+    );
+    localStorage.setItem("books", JSON.stringify(listBooksFavorite));
+  });
 }
--- js/index.js
+++ js/index.js
@@ -1,114 +1,98 @@
 import { apiKey } from "./key.js";
-const btnSearch = document.querySelector('#btnSearch');
+const btnSearch = document.querySelector("#btnSearch");
 const listBooksFavorite = JSON.parse(localStorage.getItem("books")) || [];
 
-btnSearch.addEventListener('click', (event) => {
-    event.preventDefault();
-    const input = document.querySelector('#search');
-    if (input.value == '') {
-        alert('Digite o nome do filme para pesquisar')
-        return;
-    }
+btnSearch.addEventListener("click", (event) => {
+  event.preventDefault();
 
-    searchBook(input);
-    input.value = '';
-})
+  const input = document.querySelector("#search");
+  if (input.value == "") {
+    alert("Digite o nome do filme para pesquisar");
+    return;
+  }
+
+  searchBook(input);
+  input.value = "";
+});
 
 async function searchBook(input) {
-
-    try{
-        const api = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${input.value}&maxResults=30&key=${apiKey}`);
-        const resultOfSearch = await api.json();
-        creatBook(resultOfSearch.items);
-
-    }catch(error){
-        alert("Erro ao buscar os dados da API")
-    }
-    
-
-    
+  try {
+    const api = await fetch(
+      `https://www.googleapis.com/books/v1/volumes?q=${input.value}&maxResults=15&key=${apiKey}`
+    );
+    const resultOfSearch = await api.json();
+    creatBook(resultOfSearch.items);
+  } catch (error) {
+    alert("Erro ao buscar os dados da API");
+  }
 }
 
 function creatBook(json) {
-    const list = document.querySelector('.list');
-    list.innerHTML = '';
+  const list = document.querySelector(".list");
+  list.innerHTML = "";
 
+  json.forEach((element) => {
+    if (element.volumeInfo.imageLinks != undefined) {
+      let title = element.volumeInfo.title;
+      let image = element.volumeInfo.imageLinks.smallThumbnail;
+      let idBook = element.selfLink;
 
-    json.forEach((element) => {
-
-        if (element.volumeInfo.imageLinks != undefined) {
-
-            let title = element.volumeInfo.title;
-            let image = element.volumeInfo.imageLinks.smallThumbnail;
-            let idBook = element.selfLink;
-
-            list.innerHTML += `<div class="item" id="${idBook}">
+      list.innerHTML += `<div class="item" id="${idBook}">
         <h2 class="book-title">${title}</h2>
         <img src="${image}">
-        <div>
-          <button class="show-details-button">Saiba mais</button>
-          <button data-btn-favorite>Favoritos</button>
+        <div class="section-btn">
+          <button class=" btn btn-primary show-details-button">Saiba mais</button>
+                    
+                    <button data-btn-favorite class="btn btn-outline-success">
+                        <i class="bi bi-heart"></i> 
+                    </button>
         </div>
 
         </div>`;
-
     }
-        
-    });
-    
-    listBooks()  
+  });
 
-   
-
+  listBooks();
 }
-
-
 
 function listBooks() {
+  const books = document.querySelectorAll(".item");
 
-    const books = document.querySelectorAll('.item');
+  books.forEach((book) => {
+    const detailsButton = book.querySelector(".show-details-button");
+    const btnFavorite = book.querySelector("[data-btn-favorite]");
 
-    books.forEach(book => {
-        const detailsButton = book.querySelector('.show-details-button');
-        const btnFavorite = book.querySelector('[data-btn-favorite]');
+    detailsButton.addEventListener("click", () => {
+      const idBook = book.getAttribute("id");
+      getIdBook(idBook);
+    });
 
-        detailsButton.addEventListener('click', () => {
-            const idBook = book.getAttribute('id')
-            getIdBook(idBook)
-        })
+    btnFavorite.addEventListener("click", () => {
+      const title = book.childNodes[1].textContent;
+      const idBook = book.getAttribute("id");
 
-        btnFavorite.addEventListener('click', ()=>{
-            const title = book.childNodes[1].textContent;
-            const idBook = book.getAttribute('id')
-            
-            const favoriteBook =  {
-                nameBook : title,
-                id: idBook
-            }
-            listBooksFavorite.push(favoriteBook);
-            console.log(listBooksFavorite)
-            localStorage.setItem('books', JSON.stringify(listBooksFavorite));
-            alert('Livro adicionado aos favoritos!')
-        })
-        
+      const favoriteBook = {
+        nameBook: title,
+        id: idBook,
+      };
+      listBooksFavorite.push(favoriteBook);
 
-    })
-
+      localStorage.setItem("books", JSON.stringify(listBooksFavorite));
+      alert("Livro adicionado aos favoritos!");
+    });
+  });
 }
 
-async function getIdBook(id){
-    const api = await fetch(id)
-    const resultID = await api.json();
-    openModal(resultID)
+async function getIdBook(id) {
+  const api = await fetch(id);
+  const resultID = await api.json();
+  openModal(resultID);
 }
 
-function openModal(book){
-    const modal = document.querySelector('[data-modal]')
-    modal.innerHTML = `<img src="${book.volumeInfo.imageLinks.thumbnail}" alt="">
+function openModal(book) {
+  const modal = document.querySelector("[data-modal]");
+  modal.innerHTML = `<img src="${book.volumeInfo.imageLinks.thumbnail}" alt="">
     <h2>${book.volumeInfo.title}</h2>
-    <p>${book.volumeInfo.description}</p>`
-    modal.showModal()
+    <p>${book.volumeInfo.description}</p>`;
+  modal.showModal();
 }
-
-
-
